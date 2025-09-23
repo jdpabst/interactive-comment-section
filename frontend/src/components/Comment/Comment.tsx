eae825e9fc856e92d684320ffef306fef03b9a51
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-09-23 15:30:04 -0600

    mobile friendly main stuff

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -4,7 +4,10 @@ import './comment.scss';
 export default function Comment() {
  return (
   <div className='main-comment-container'>
-   <Upvote />
+   <div className='upvote-desktop'>
+    <Upvote />
+   </div>
+
    <div className='comment-contents-container'>
     <div className='comment-header display-flex-row'>
      <div className='user-and-comment-info display-flex-row'>
@@ -19,6 +22,14 @@ export default function Comment() {
     </div>
     <div className='comment'>
      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it.</p>
+    </div>
+
+    <div className="comment-actions">
+     <Upvote />
+     <button className="reply-bttn display-flex-row">
+      <img src="/assets/icons/icon-reply.svg" />
+      <p>Reply</p>
+     </button>
     </div>
    </div>
   </div>
